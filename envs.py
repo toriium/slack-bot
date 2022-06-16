e44ed9224020e46a51146c97447b7828948ed18f
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-06-16 18:59:27 -0300

    feat: values from env

---
+++ envs.py
@@ -0,0 +1,3 @@
+from decouple import config
+
+SLACK_WEBHOOK_URL = config("SLACK_WEBHOOK_URL")
--- slack_bot/bot.py
+++ slack_bot/bot.py
@@ -1,7 +1,6 @@
 from slack_sdk.webhook import WebhookClient
 
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+from envs import SLACK_WEBHOOK_URL
 
 webhook_client = WebhookClient(url=SLACK_WEBHOOK_URL)
-
 webhook_client.send(text=f'@here \n ola mundo')
