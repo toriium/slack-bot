6dd4d1dd3e088ddc2f89db2344fe1ad99af24423
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-06-26 16:21:49 -0300

    feat: SLACK_BOT_TOKEN

--- envs.py
+++ envs.py
@@ -1,13 +1,22 @@
+import os
+from pathlib import Path
+
 from pydantic import BaseSettings
+
+ENV_PATH = os.path.dirname(os.path.realpath(__file__))
+ENV_PATH = Path(ENV_PATH + '/env.env')
 
 
 class Settings(BaseSettings):
     SLACK_WEBHOOK_URL: str
     SLACK_BOT_TOKEN: str
+    SLACK_USER: str
 
     class Config:
-        env_file = '../env.env'
+        env_file = ENV_PATH
         env_file_encoding = 'utf-8'
 
 
 ENV = Settings()
+# CHANNEL = "#programming"
+CHANNEL = "C03L0LPBYD9"
