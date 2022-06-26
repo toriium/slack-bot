9a507ffe676a91eb770d6145e25436bf5f067365
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-06-26 15:02:21 -0300

    feat: /web_hook_client

--- slack_bot/web_hook_client/web_hook_client.py
+++ slack_bot/web_hook_client/web_hook_client.py
@@ -1,6 +1,6 @@
 from slack_sdk.webhook import WebhookClient
 
-from envs import SLACK_WEBHOOK_URL
+from envs import ENV
 
-webhook_client = WebhookClient(url=SLACK_WEBHOOK_URL)
+webhook_client = WebhookClient(url=ENV.SLACK_WEBHOOK_URL)
 webhook_client.send(text=f'@here \n ola mundo')
