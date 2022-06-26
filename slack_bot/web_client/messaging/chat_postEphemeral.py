eb9da54f1ecc29adaf593d9efea754b03b76a0e8
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-06-26 16:18:18 -0300

    feat: SLACK_USER

--- slack_bot/web_client/messaging/chat_postEphemeral.py
+++ slack_bot/web_client/messaging/chat_postEphemeral.py
@@ -6,4 +6,4 @@ client = WebClient(token=ENV.SLACK_BOT_TOKEN)
 
 response = client.chat_postEphemeral(channel=CHANNEL,
                                      text="Chat menssage to a specific person",
-                                     user=ENV.SLACK_PERSONAL_USER, )
+                                     user=ENV.SLACK_USER, )
