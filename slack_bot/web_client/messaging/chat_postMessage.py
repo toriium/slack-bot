8eabb173a1fa2cf7e69301e2c9794deb232a91e3
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-06-26 15:33:00 -0300

    feat: chat_postMessage

---
+++ slack_bot/web_client/messaging/chat_postMessage.py
@@ -0,0 +1,18 @@
+from slack_sdk import WebClient
+
+from envs import ENV, CHANNEL
+
+client = WebClient(token=ENV.SLACK_BOT_TOKEN)
+
+response = client.chat_postMessage(channel=CHANNEL,
+                                   text="Sending a normal chat menssage")
+ts = response['ts']
+response = client.chat_postMessage(channel=CHANNEL,
+                                   text="Repliplyng chat menssage",
+                                   thread_ts=ts)
+
+ts = response['ts']
+response = client.chat_postMessage(channel=CHANNEL,
+                                   text="Repliplyng chat menssage with: replied to a thread:",
+                                   thread_ts=ts,
+                                   reply_broadcast=True)
